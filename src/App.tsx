da0aae897a4f675f39dbcc7e8d82d6e72862e8e7
Kaio <kaio_matos22@hotmail.com>
2021-11-13 10:31:53 -0300

    Initial structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Título</h1>
+      <p>Texto</p>
+      <p className="subtext">Subtexto</p>
+    </div>
+  );
+}
+
+export default App;
