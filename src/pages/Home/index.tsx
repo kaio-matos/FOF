4650cdf4d4fe6f3b072902fc5d07a3e8058e39e1
Kaio <kaio_matos22@hotmail.com>
2021-11-13 12:15:29 -0300

    [Home] FIx typo hero message

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,12 +8,13 @@ export default function Home() {
       <section className="hero">
         <div>
           <h1>Help us build a culture of helpers</h1>
-          <p>We're are doing something about it</p>
+          <p>We're doing something about it</p>
           <Link className="button" to="/about">
             See how
           </Link>
         </div>
       </section>
+      <section></section>
     </main>
   );
 }
