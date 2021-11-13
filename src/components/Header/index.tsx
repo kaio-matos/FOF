fcbc2d7b2ec3f1de2e2d50c5358aadecc826c21b
Kaio <kaio_matos22@hotmail.com>
2021-11-13 11:27:48 -0300

    Adding Header styles, chaging HTML and General CSS

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,28 @@
 import { Link } from "react-router-dom";
+import { BiSearch } from "react-icons/bi";
 import "./styles.css";
 
 export default function Header() {
   return (
     <header className="header">
-      <div className="header_logo"></div>
-      <div className="header_menu">
+      <div className="header_logo">
+        <h1>Logo</h1>
+      </div>
+      <nav className="header_menu text">
         <ul>
           <li>
             <Link to="/">Home</Link>
+          </li>
+          <li>
             <Link to="/about">About FOF</Link>
           </li>
         </ul>
+      </nav>
+      <div className="header_search">
+        <button>
+          <BiSearch fontSize="1.3rem" />
+        </button>
       </div>
-      <div className="header_search"></div>
     </header>
   );
 }
