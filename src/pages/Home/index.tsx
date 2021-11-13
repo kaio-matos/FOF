b9367a0af082f168b5552f2b66ea6b3b59ce3373
Kaio <kaio_matos22@hotmail.com>
2021-11-13 12:11:50 -0300

    [Home] Adding hero to homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,19 @@
 import "./styles.css";
+import "./styles.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <main>
+      <section className="hero">
+        <div>
+          <h1>Help us build a culture of helpers</h1>
+          <p>We're are doing something about it</p>
+          <Link className="button" to="/about">
+            See how
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
 }
