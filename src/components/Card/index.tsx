574714a08e371f722e91caf9cd93044461496cd1
Kaio <kaio_matos22@hotmail.com>
2021-11-25 16:12:46 -0300

    [Card] Improving code

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,44 +4,45 @@ import "./styles.css";
 
 export default function Card({ project }: { project: projectType }) {
   const navigate = useNavigate();
+  const fundStatus = Number(project.funding._text) < Number(project.goal._text);
+
+  function openProjectPage() {
+    navigate(`/FOF/projects/${project.id._text}`);
+  }
+
+  function collapseString(string: string, collapseWhen = 150) {
+    return string.slice(0, collapseWhen) + "...";
+  }
+
+  function getExistingImage(project: projectType) {
+    return project.organization
+      ? project.organization?.logoUrl?._text
+      : project.imageLink._text;
+  }
 
   return (
-    <div
-      onClick={() => {
-        navigate(`/FOF/projects/${project.id._text}`);
-      }}
-      className="card"
-    >
+    <div onClick={openProjectPage} className="card">
       <div className="card_image_container">
-        <img
-          src={
-            project.organization
-              ? project.organization?.logoUrl?._text
-              : project.imageLink._text
-          }
-          alt={project.title._text}
-        />
+        <img src={getExistingImage(project)} alt={project.title._text} />
       </div>
+
       <div className="card_info">
         <div className="card_title">
           <span className="title">{project.title._text}</span>
           <span className="title">{project.themeName._text}</span>
         </div>
+
         <div className="card_data">
-          <p>{project.summary._text.slice(0, 150) + "..."}</p>
+          <p>{collapseString(project.summary._text)}</p>
 
           <div className="card_money">
-            <p
-              className={
-                Number(project.funding._text) < Number(project.goal._text)
-                  ? "needFunds"
-                  : "completeFunds"
-              }
-            >
-              <span>$ {project.funding._text}</span> <sub>funding</sub>
+            <p className={fundStatus ? "needFunds" : "completeFunds"}>
+              <span>$ {project.funding._text}</span>
+              <sub>funding</sub>
             </p>
             <p className="goal">
-              <span>$ {project.goal._text}</span> <sub>goal</sub>
+              <span>$ {project.goal._text}</span>
+              <sub>goal</sub>
             </p>
           </div>
         </div>
