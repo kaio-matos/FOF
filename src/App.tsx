6d34c34ba9723ea9531dfd09e13d80a21e0d747e
Kaio <kaio_matos22@hotmail.com>
2021-11-27 13:44:24 -0300

    [Error404] Adding not found page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Project from "./pages/Project";
+import Error404 from "./pages/Error404";
 import { APIContextProvider } from "./contexts/APIContext";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
             <Route path="projects/:id" element={<Project />} />
             <Route path="" element={<Home />} />
           </Route>
+          <Route path="/*" element={<Error404 />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/pages/Error404/index.tsx
@@ -0,0 +1,17 @@
+import Hero from "../../components/Hero";
+
+export default function Error404() {
+  return (
+    <main>
+      <Hero
+        title="Page not found"
+        paragraph=""
+        link={{
+          url: "FOF",
+          text: "Go to Home",
+        }}
+        image="https://source.unsplash.com/random"
+      />
+    </main>
+  );
+}
